ff283b1015eb866947debbbbfaecd6a64f39ace5
tobiade <tobi_adewuyi@yahoo.com>
2016-01-25 17:18:16 +0000

    Initial Commit!

---
+++ SensorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SensorApp
+//
+//  Created by Adewuyi, Oloruntobi on 25/01/2016.
+//  Copyright © 2016 Adewuyi, Oloruntobi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
