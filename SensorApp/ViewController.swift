3df5530dd2cdf373f36749dd8be8ae81d9300fbc
Sarah-Jessica Jemitola <sarah-jessicajemitola@sarahjesicasmbp.lan>
2016-01-26 00:58:26 +0000

    Accelerometer dev

--- SensorApp/ViewController.swift
+++ SensorApp/ViewController.swift
@@ -9,16 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var xAccelerometerValue: UILabel!
+    @IBOutlet weak var yAccelerometerValue: UILabel!
+    @IBOutlet weak var zAccelerometerValue: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
