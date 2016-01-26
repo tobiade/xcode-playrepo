31bf9f37f3560257a7aa930b31ff48854399f277
Sarah-Jessica Jemitola <sarah-jessicajemitola@sarahjesicasmbp.lan>
2016-01-26 02:22:34 +0000

    Added motion kit stuff. For some reason it is taking ages to update ui with accelerometer values :/

--- SensorApp/ViewController.swift
+++ SensorApp/ViewController.swift
@@ -15,14 +15,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var xAccelerometerValue: UILabel!
     @IBOutlet weak var yAccelerometerValue: UILabel!
     @IBOutlet weak var zAccelerometerValue: UILabel!
-
+   
+    let motionkit = MotionKit()
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     
-
-
+  // MARK: Actions
+    
+    @IBAction func accBttn(sender: UIButton) {
+//        motionkit.getAccelerometerValues(0.5) { (x, y, z) -> () in
+//            self.xAccelerometerValue.text=String(x)
+//            self.yAccelerometerValue.text=String(y)
+//            self.zAccelerometerValue.text=String(z)
+//        }
+        self.xAccelerometerValue.text="YAhhhhh"
+        self.yAccelerometerValue.text="Heyyyyyy"
+        self.zAccelerometerValue.text="Looooool"
+    }
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
